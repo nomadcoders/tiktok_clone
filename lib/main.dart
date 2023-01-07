8659e7d2e6b5a01efc839ff88074215917b2e588
니꼬 <nico@nomadcoders.co>
2023-01-07 15:16:39 +0700

    1.0 Initialization

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: Container(),
+    );
+  }
+}
