dcfb8aff05e481c5569a5d1f10e872274e8a09c7
니꼬 <nico@nomadcoders.co>
2023-01-25 16:42:54 +0700

    9.4 Chat Detail

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class ChatDetailScreen extends StatefulWidget {
   const ChatDetailScreen({super.key});
@@ -12,6 +13,109 @@ class ChatDetailScreen extends StatefulWidget {
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: ListTile(
+          contentPadding: EdgeInsets.zero,
+          horizontalTitleGap: Sizes.size8,
+          leading: const CircleAvatar(
+            radius: Sizes.size24,
+            foregroundImage: NetworkImage(
+              "https://avatars.githubusercontent.com/u/3612017",
+            ),
+            child: Text('니꼬'),
+          ),
+          title: const Text(
+            '니꼬',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          subtitle: const Text('Active now'),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              FaIcon(
+                FontAwesomeIcons.flag,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+              Gaps.h32,
+              FaIcon(
+                FontAwesomeIcons.ellipsis,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          ListView.separated(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size20,
+              horizontal: Sizes.size14,
+            ),
+            itemBuilder: (context, index) {
+              final isMine = index % 2 == 0;
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment:
+                    isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(Sizes.size14),
+                    decoration: BoxDecoration(
+                      color:
+                          isMine ? Colors.blue : Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(
+                          Sizes.size20,
+                        ),
+                        topRight: const Radius.circular(
+                          Sizes.size20,
+                        ),
+                        bottomLeft: Radius.circular(
+                          isMine ? Sizes.size20 : Sizes.size5,
+                        ),
+                        bottomRight: Radius.circular(
+                          !isMine ? Sizes.size20 : Sizes.size5,
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "this is a message!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+            separatorBuilder: (context, index) => Gaps.v10,
+            itemCount: 10,
+          ),
+          Positioned(
+            bottom: 0,
+            width: MediaQuery.of(context).size.width,
+            child: BottomAppBar(
+              color: Colors.grey.shade50,
+              child: Row(
+                children: [
+                  const Expanded(child: TextField()),
+                  Gaps.h20,
+                  Container(
+                    child: const FaIcon(FontAwesomeIcons.paperPlane),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
