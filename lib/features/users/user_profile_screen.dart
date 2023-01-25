8bf45d93f0df45a9241e42e22c952f9d97d35ebc
니꼬 <nico@nomadcoders.co>
2023-01-25 20:18:27 +0700

    10.2 SliverAppBar

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -13,9 +13,10 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
+          snap: true,
           floating: true,
-          stretch: true,
           pinned: true,
+          stretch: true,
           backgroundColor: Colors.teal,
           collapsedHeight: 80,
           expandedHeight: 200,
@@ -23,6 +24,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             stretchModes: const [
               StretchMode.blurBackground,
               StretchMode.zoomBackground,
+              StretchMode.fadeTitle,
             ],
             background: Image.asset(
               "assets/images/placeholder.jpg",
@@ -30,6 +32,19 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
             title: const Text("Hello!"),
           ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
+          ),
+          itemExtent: 100,
         )
       ],
     );
