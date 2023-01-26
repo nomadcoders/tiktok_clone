f3fb20ce21add98aeea9307639a8f1d9c95286e6
니꼬 <nico@nomadcoders.co>
2023-01-26 17:50:18 +0700

    11.5 CupertinoActionSheet

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -121,6 +121,31 @@ class _SettingsScreenState extends State<SettingsScreen> {
               );
             },
           ),
+          ListTile(
+            title: const Text("Log out (iOS / Bottom)"),
+            textColor: Colors.red,
+            onTap: () {
+              showCupertinoModalPopup(
+                context: context,
+                builder: (context) => CupertinoActionSheet(
+                  title: const Text("Are you sure?"),
+                  message: const Text("Please dooooont gooooo"),
+                  actions: [
+                    CupertinoActionSheetAction(
+                      isDefaultAction: true,
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text("Not log out"),
+                    ),
+                    CupertinoActionSheetAction(
+                      isDestructiveAction: true,
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text("Yes plz."),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
           const AboutListTile(
             applicationVersion: "1.0",
             applicationLegalese: "Don't copy me.",
