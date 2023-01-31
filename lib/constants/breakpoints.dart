3f43e290961436b9cf7993f1f0d37623642946e1
니꼬 <nico@nomadcoders.co>
2023-01-31 16:13:37 +0700

    12.5 ConstrainedBox

--- lib/constants/breakpoints.dart
+++ lib/constants/breakpoints.dart
@@ -1,7 +1,7 @@
 class Breakpoints {
-  static const sm = 640;
-  static const md = 768;
-  static const lg = 1024;
-  static const xl = 1280;
-  static const xxl = 1536;
+  static const sm = 640.0;
+  static const md = 768.0;
+  static const lg = 1024.0;
+  static const xl = 1280.0;
+  static const xxl = 1536.0;
 }
--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -49,10 +49,15 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           elevation: 1,
-          title: CupertinoSearchTextField(
-            controller: _textEditingController,
-            onChanged: _onSearchChanged,
-            onSubmitted: _onSearchSubmitted,
+          title: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: Breakpoints.sm,
+            ),
+            child: CupertinoSearchTextField(
+              controller: _textEditingController,
+              onChanged: _onSearchChanged,
+              onSubmitted: _onSearchSubmitted,
+            ),
           ),
           bottom: TabBar(
             splashFactory: NoSplash.splashFactory,
