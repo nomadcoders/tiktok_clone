a76f5168c40365cca9f0bfc6c2aec7963a05bbd5
니꼬 <nico@nomadcoders.co>
2023-02-26 17:53:12 +0900

    26.2 StreamProvider

--- lib/features/inbox/models/message.dart
+++ lib/features/inbox/models/message.dart
@@ -1,20 +1,24 @@
 class MessageModel {
   final String text;
   final String userId;
+  final int createdAt;
 
   MessageModel({
     required this.text,
     required this.userId,
+    required this.createdAt,
   });
 
   MessageModel.fromJson(Map<String, dynamic> json)
       : text = json['text'],
+        createdAt = json['createdAt'],
         userId = json['userId'];
 
   Map<String, dynamic> toJson() {
     return {
       "text": text,
       "userId": userId,
+      "createdAt": createdAt,
     };
   }
 }
--- lib/features/inbox/view_models/messages_view_model.dart
+++ lib/features/inbox/view_models/messages_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_clone/features/inbox/models/message.dart';
@@ -20,8 +21,9 @@ class MessagesViewModel extends AsyncNotifier<void> {
       final message = MessageModel(
         text: text,
         userId: user!.uid,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
       );
-      _repo.sendMessage(message);
+      await _repo.sendMessage(message);
     });
   }
 }
@@ -29,3 +31,23 @@ class MessagesViewModel extends AsyncNotifier<void> {
 final messagesProvider = AsyncNotifierProvider<MessagesViewModel, void>(
   () => MessagesViewModel(),
 );
+
+final chatProvider = StreamProvider<List<MessageModel>>((ref) {
+  final db = FirebaseFirestore.instance;
+
+  return db
+      .collection("chat_rooms")
+      .doc("5wIEF4qSGhFMKms4Pjuu")
+      .collection("texts")
+      .orderBy("createdAt")
+      .snapshots()
+      .map(
+        (event) => event.docs
+            .map(
+              (doc) => MessageModel.fromJson(
+                doc.data(),
+              ),
+            )
+            .toList(),
+      );
+});
