a296eaed30d7b2d6901270cb12dc8174f1edaed5
니꼬 <nico@nomadcoders.co>
2023-02-26 22:51:03 +0900

    27.2 Foreground Notifications

--- lib/features/notifications/notifications_provider.dart
+++ lib/features/notifications/notifications_provider.dart
@@ -14,11 +14,24 @@ class NotificationsProvider extends AsyncNotifier {
     await _db.collection("users").doc(user!.uid).update({"token": token});
   }
 
+  Future<void> initListeners() async {
+    final permission = await _messaging.requestPermission();
+    if (permission.authorizationStatus == AuthorizationStatus.denied) {
+      return;
+    }
+    // Foreground
+    FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+      print("I just got a message and I'm in the foreground");
+      print(event.notification?.title);
+    });
+  }
+
   @override
   FutureOr build() async {
     final token = await _messaging.getToken();
     if (token == null) return;
     await updateToken(token);
+    await initListeners();
     _messaging.onTokenRefresh.listen((newToken) async {
       await updateToken(newToken);
     });
